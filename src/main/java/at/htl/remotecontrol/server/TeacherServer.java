24d0649332412ca349874fc45db36ddf6ec47d57
Philipp <philipp.hermueller@gmx.at>
2015-10-26 19:34:42 +0100

    Merge branch 'master' of /Users/Philipp/Desktop/Testumgebung/Testumgebung1 with conflicts.

--- src/main/java/at/htl/remotecontrol/server/TeacherServer.java
+++ src/main/java/at/htl/remotecontrol/server/TeacherServer.java
@@ -1,59 +1,98 @@
 package at.htl.remotecontrol.server;
 
-import at.htl.remotecontrol.entity.Image;
-import at.htl.remotecontrol.entity.Student;
+import at.htl.remotecontrol.entity.Time;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.time.LocalDateTime;
 
-/**
- * Philipp:  21.10.2015   Einfügen der "saveImage()"-Methode zum speichern der Screenshots
- * Tobias :  26.10.2015   Verbesserung der von saveImage()
- */
 public class TeacherServer {
+  public static final int PORT = 5555;
 
-    public static final int PORT = 5555;
+  private final SocketWriterThread writer;
+  private final SocketReaderThread reader;
 
-    private final SocketWriterThread writer;
-    private final SocketReaderThread reader;
+  public TeacherServer(Socket socket)
+      throws IOException, ClassNotFoundException {
+    ObjectOutputStream out = new ObjectOutputStream(
+        socket.getOutputStream());
+    ObjectInputStream in = new ObjectInputStream(
+        new BufferedInputStream(
+            socket.getInputStream()));
+    System.out.println("waiting for student name ...");
+    String studentName = (String) in.readObject();
 
-    public TeacherServer(Socket socket)
-            throws IOException, ClassNotFoundException {
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(
-                new BufferedInputStream(
-                        socket.getInputStream()));
-        System.out.println("waiting for student name ...");
 
-        Student student = new Student((String) in.readObject());
-        reader = new SocketReaderThread(student, in, this);
-        writer = new SocketWriterThread(student, out);
+    reader = new SocketReaderThread(studentName, in, this);
+    writer = new SocketWriterThread(studentName, out);
 
-        reader.setDaemon(true);
-        writer.setDaemon(true);
+    reader.setDaemon(true);
+    writer.setDaemon(true);
 
-        reader.start();
-        writer.start();
+    reader.start();
+    writer.start();
 
-        System.out.println("finished connecting to " + socket);
+    System.out.println("finished connecting to " + socket);
+  }
+
+  public void saveImage(BufferedImage image, String studentname) {
+
+    LocalDateTime dateTime = LocalDateTime.now();
+
+    FileOutputStream fos = null;
+    try {
+        File f = new File(Time.getInstance().getScreenshotPath() + "/Screenshots/" + studentname);
+        if(!f.exists()) {
+            f.mkdirs();
+        }
+
+
+
+        fos = new FileOutputStream(f.getPath() +
+                "/" +studentname +
+                "-" +
+                dateTime +
+                ".jpg"
+        );
+
+      fos.write(convertToJPG(image));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        fos.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
-    public void saveImage(BufferedImage image, Student student) {
-        String path = String.format("%s/%s-%s.jpg",
-                student.getDirectory(),
-                student.getName(),
-                LocalDateTime.now());
-        Image.save(image, path);
-    }
+  }
 
-    public void shutdown() {
-        writer.interrupt();
-        reader.close();
-    }
+  private byte[] convertToJPG(BufferedImage img)
+          throws IOException {
+    ImageWriter writer =
+            ImageIO.getImageWritersByFormatName("jpg").next();
+    ImageWriteParam iwp = writer.getDefaultWriteParam();
+    iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    iwp.setCompressionQuality(1.0f);
 
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    writer.setOutput(new MemoryCacheImageOutputStream(bout));
+    writer.write(null, new IIOImage(img, null, null), iwp);
+    writer.dispose();
+    bout.flush();
+    return bout.toByteArray();
+  }
+
+  public void shutdown() {
+    writer.interrupt();
+    reader.close();
+  }
 }
