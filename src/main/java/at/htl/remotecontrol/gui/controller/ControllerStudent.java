0e95c6d79a5e72b48795293231b1e44c4ca6cf25
gnadlinger <johannes.gnadlinger1997@gmail.com>
2015-10-29 11:08:25 +0100

    Version 1.0.5.012
    Hinzufügen eines Buttons in Student.fxml
    und anlegen eines Ordners mit dem Namen des Schülers.

--- src/main/java/at/htl/remotecontrol/gui/controller/ControllerStudent.java
+++ src/main/java/at/htl/remotecontrol/gui/controller/ControllerStudent.java
@@ -1,13 +1,18 @@
 package at.htl.remotecontrol.gui.controller;
 
 import at.htl.remotecontrol.Client;
+import at.htl.remotecontrol.entity.Session;
+import at.htl.remotecontrol.entity.TestDirectoryCooser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,6 +28,9 @@ public class ControllerStudent implements Initializable {
     @FXML
     Button btnLogin;
 
+    @FXML
+    Button btfilePath;
+
     private Client client;
 
     public ControllerStudent() {
@@ -34,12 +42,23 @@ public class ControllerStudent implements Initializable {
     public void logIn(ActionEvent event) {
         try {
             client = new Client(tfTeacherIP.getText(), tfUsername.getText());
+            TestDirectoryCooser.getInstance().setUsername(tfUsername.getText());
             client.start();
         } catch (AWTException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void fileDirectory(ActionEvent event){
+
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setInitialDirectory(new File(System.getProperty("user.home")));
+        dc.setTitle("Wähle dein Ziel-Verzeichnis");
+        File choosedFile = dc.showDialog(new Stage());
+        TestDirectoryCooser.getInstance().setFilePath(choosedFile.getPath());
+        TestDirectoryCooser.getInstance().CreateDirectory();
     }
 
     public void logOut(ActionEvent actionEvent) {
